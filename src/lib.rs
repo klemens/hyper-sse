a3cec1b571260a15d08995f7b935f6f2fa80bc42
Klemens Schölhorn <klemens@schoelhorn.eu>
2018-07-10 02:10:13 +0200

    Disable buffering for older nginx versions

--- src/lib.rs
+++ src/lib.rs
@@ -140,6 +140,7 @@ impl<C: DeserializeOwned + Eq + Hash + FromStr + Send + Serialize> Server<C> {
 
         Response::builder()
             .header("Cache-Control", "no-cache")
+            .header("X-Accel-Buffering", "no")
             .header("Content-Type", "text/event-stream")
             .header("Access-Control-Allow-Origin", "*")
             .body(body)
